ff6c0d157d216907014d6d45f840d620423facdd
marianivna8 <front12end@gmail.com>
2022-07-17 01:42:07 +0300

    responsive

--- 15-hw/prod/js/main.js
+++ 15-hw/prod/js/main.js
@@ -17,6 +17,33 @@ $(function() {
     $('.review-slider').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed: 1000,
+        speed:5000,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                arrows: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                dots: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     })
 })
