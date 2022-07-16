a42d0a6d9fbd91aac2af3fe01bbdebb04732c4f3
marianivna8 <front12end@gmail.com>
2022-07-17 00:55:09 +0300

    add slider

--- 15-hw/prod/js/main.js
+++ 15-hw/prod/js/main.js
@@ -1,3 +1,22 @@
+
 'use strict';
 
 console.log('test');
+
+let loader = document.querySelector('.loader');
+
+window.addEventListener('load', () =>{
+    loader.classList.add('hide');
+    setTimeout(() => {
+        loader.remove();
+    }, 600);
+});
+
+$(function() {
+
+    $('.review-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    })
+})
